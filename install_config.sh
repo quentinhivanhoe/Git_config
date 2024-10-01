83c6632b41ee2982723c31e56dbc61b7f00dc603
quentinhivanhoe <quentin.hivanhoe@epitech.eu>
2024-10-01 11:26:53 +0400

    [FEAT]: add ci-am alias for a quick commit for modified files

--- install_config.sh
+++ install_config.sh
@@ -5,8 +5,34 @@
 # set alias to a prettier git log
 # log the abrieve hash commits, commit author, commit relaive date and the suject of the commit
 # example : 74c8b9e - GitHub, 30 minutes ago : Initial commit
-git config --global --replace-all alias.lg 'log --pretty=format:"%h - %cn, %cr : %s"'
+git config --global --replace-all alias.lg 'log --decorate --pretty=format:"%h - %cn, %cr : %s"'
 
-# alias rstr-stgd
+# alias rst-stg
 # set alias to unstaged file
-git config --global --replace-all alias.rstr-stgd 'restore --staged'
+# usefull to remove a unwanted file from the staged hunk 
+git config --global --replace-all alias.rst-stg 'restore --staged'
+
+# alias st
+# set alias to see short status
+# usefull to check quickly wich file is staged/unstaged/modifed/untracked
+git config --global --replace-all alias.st 'status -s'
+
+# alias df-stg
+# set alias to see diff between remote files and local staged files
+# usefull to see the difference just before a commit
+git config --global --replace-all alias.df-stg 'diff --staged'
+
+# alias df
+# set alias to see diff for unstaged files
+# usefull to see what we staging
+git config --global --replace-all alias.df 'diff'
+
+# alias ci
+# set alias to commit your local change
+# usefull for a quick commit
+git config --global --replace-all alias.ci 'commit -m'
+
+# alias ci-am
+# set alias to commit tracked files
+# usefull to commit only modified files
+git config --global --replace-all alias.ci-am 'commit -am'
